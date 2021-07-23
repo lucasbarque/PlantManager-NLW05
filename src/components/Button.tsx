d011081fd9f2b54f14f2650b53a75eda66738d8a
Lucas Barque da Silva <lucas.barque@sesims.com.br>
2021-07-22 21:15:49 -0400

    NLW Day 2

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,42 +1,42 @@
 import React from 'react';
+import { 
+  TouchableOpacity, 
+  Text,
+  StyleSheet,
+  TouchableOpacityProps  
+ } from 'react-native';
 
-import { TouchableOpacity, Text, StyleSheet, TouchableOpacityProps } from 'react-native';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 interface ButtonProps extends TouchableOpacityProps{
   title: string;
 }
 
-export function Button({ title, ...rest }: ButtonProps ) {
-  return(
-    <>
-    <TouchableOpacity
-        style={styles.button}
-        activeOpacity={0.7}
-        {...rest}
-      >
-        <Text style={styles.buttonText}>
-          { title }
-        </Text>
-      </TouchableOpacity>
-      </>
+export function Button({ title, ...rest } : ButtonProps) {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <Text 
+        style={styles.text}
+        { ...rest }
+        >
+        { title }
+      </Text>
+    </TouchableOpacity>
   );
 }
 
-
-const styles = StyleSheet.create({   
-  button: {
+const styles = StyleSheet.create({
+  container: {
     backgroundColor: colors.green,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 16,
-    marginBottom: 10,
     height: 56,
-    width: 56,
-    paddingHorizontal: 20    
-  }, 
-  buttonText: {
+    borderRadius: 16,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 16,
     color: colors.white,
-    fontSize: 24
+    fontFamily: fonts.heading
   }
 });
